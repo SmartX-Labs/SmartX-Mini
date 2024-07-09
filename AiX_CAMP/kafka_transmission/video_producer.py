fc99434c994bd7138baa27b67745835987a949d8
minjunj <minjun_jo@gm.gist.ac.kr>
2024-07-09 16:54:23 +0900

    update : change to use picam

--- AiX_CAMP/kafka_transmission/video_producer.py
+++ AiX_CAMP/kafka_transmission/video_producer.py
@@ -1,29 +1,25 @@
-import time
-import sys
-import argparse
-import cv2
-import os
-
+from picamera2 import Picamera2, MappedArray
+import io
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
+import time
+import argparse
+import os
 
-# 카메라 영상을 스트리밍합니다
-def produce_videostream(device_path):
-    print("비디오 전송을 시작합니다")
-    video = cv2.VideoCapture(device_path)
+def main():
+    # Initialize the camera
+    picam2 = Picamera2()
+    config = picam2.create_preview_configuration(main={"format": "RGB888"})
+    picam2.configure(config)
+    picam2.start()
 
-    # 비디오를 읽어옵니다
-    while video.isOpened():
-        ret, frame = video.read()
-        if not ret:
-            print("비디오를 읽는 과정에서 오류가 발생했습니다")
-            break
+    while True:
+        # Capture frame
+        buffer = io.BytesIO()
+        picam2.capture_file(buffer, format='jpeg')
         
-        # 이미지 파일을 전송하기 위해 byte로 인코딩합니다
-        data = cv2.imencode('.jpeg', frame)[1].tobytes()
-
-        # 이미지 데이터를 전송합니다
-        future = producer.send(topic, data)
+        # transfor data
+        future = producer.send(topic, buffer.getvalue())
         try:
             future.get(timeout=10)
         except KafkaError as e:
@@ -31,8 +27,11 @@ def produce_videostream(device_path):
             break
         print('.', end='', flush=True)
 
+    # Cancel the sending task and stop the camera
+    picam2.stop()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--topic", default='pi-video')
     args = parser.parse_args()
@@ -44,8 +43,4 @@ if __name__ == "__main__":
     # Kafka producer를 선언합니다
     producer = KafkaProducer(bootstrap_servers=f"{server_ip}:9092")
 
-    # 0번째 카메라 장치를 사용해 비디오를 스트리밍합니다
-    produce_videostream(0)
-
-
-
+    main()
